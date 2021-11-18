6f565690a4f090986ac459cc816fc78d1c4df4d7
Mickey <krikkemd@gmail.com>
2021-11-18 09:38:04 +0100

    added a timer to toggle display movie start & end times

--- frontend/src/Components/Movies.js
+++ frontend/src/Components/Movies.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/style.css';
 
 const Movies = ({ moviesArray, timeOfDay, timeColor }) => {
+  const [displayEndTimes, setDisplayEndTimes] = useState(false);
+
+  const changeDisplayTimes = () => {
+    setTimeout(() => {
+      setDisplayEndTimes(!displayEndTimes);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    changeDisplayTimes();
+  }, [displayEndTimes]);
+
   return (
     <div>
       {moviesArray.length > 0 && (
@@ -13,13 +25,23 @@ const Movies = ({ moviesArray, timeOfDay, timeColor }) => {
         {moviesArray.map(movie => (
           <li className='movies__beganegrond-list-item' key={movie.id}>
             <p className='movies__beganegrond-list-item-artiest'>{movie.title}</p>
-            <p
-              className='movies__beganegrond-list-item-nummer'
-              style={{ backgroundColor: timeColor }}>
-              {[...movie.startTimes].length > 1
-                ? [...movie.startTimes].toString().replaceAll(',', ' | ')
-                : [...movie.startTimes]}
-            </p>
+            {displayEndTimes !== true ? (
+              <p
+                className='movies__beganegrond-list-item-nummer'
+                style={{ backgroundColor: timeColor }}>
+                {[...movie.startTimes].length > 1
+                  ? [...movie.startTimes].toString().replaceAll(',', ' | ')
+                  : [...movie.startTimes]}
+              </p>
+            ) : (
+              <p
+                className='movies__beganegrond-list-item-nummer'
+                style={{ backgroundColor: timeColor }}>
+                {[...movie.endTimes].length > 1
+                  ? [...movie.endTimes].toString().replaceAll(',', ' | ')
+                  : [...movie.endTimes]}
+              </p>
+            )}
           </li>
         ))}
       </ul>
